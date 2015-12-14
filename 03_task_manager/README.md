4b0eeb2e81190d2d972f4ab8889a0474ea0fe1ac
Israel Vicars <israelvicars@gmail.com>
2015-12-14 02:48:28 -0500

    Finished task manager project

--- 03_task_manager/README.md
+++ 03_task_manager/README.md
@@ -22,24 +22,28 @@ categories: [
 		"category_name": String,
 	}
 ]
-```
 
-```
 tasks: [
 	{
-		
+		"task_name": String,
+		"category": String,
+		"due_date": String,
+		"priority": String
 	}
 ]
 ```
 
-### Tasks
+### Steps
 
 1. MongoLab (ML) account setup
 2. Deploy db on ML
 3. Create db user on ML
-4. Manually create collection, document on ML
+4. Manually create categories collection on ML
 5. Stub html files add bootstrap
-6. Generate ML API key and example db docs (categories)
-7. For task categories, create GET and POST requests using jQuery
-
-99. Regenerate API key
+6. Generate ML API key
+7. For categories, create GET and POST requests using jQuery
+8. For categories, create PUT and DELETE requests using jQuery4.
+9. Manually create categories collection on ML
+10. For tasks, create GET and POST requests using jQuery
+11. For tasks, create PUT and DELETE requests using jQuery
+12. Regenerate ML API key after committing changes
--- 03_task_manager/js/main.js
+++ 03_task_manager/js/main.js
@@ -1,13 +1,173 @@
 $(document).ready(function(){
 	$('#add_category').submit(addCategory)
 	$('#edit_category').submit(editCategory)
+
+	$('#add_task').submit(addTask)
+	$('#edit_task').submit(editTask)
+
 	$('body').on('click', '.btn-edit-category', setCategory)
 	$('body').on('click', '.btn-delete-category', deleteCategory)
+
+	$('body').on('click', '.btn-edit-task', setTask)
+	$('body').on('click', '.btn-delete-task', deleteTask)
 })
 
 var api_key = 'iKz773doNzYyKq4huh0F2u4QN2R7zERZ'
 
-// GET REQUEST - Categories
+// POST REQUEST - Task
+function addTask(){
+	var task_name = $('#task_name').val()
+	var category = $('#category').val()
+	var due_date = $('#due_date').val()
+	var priority = $('#priority').val()
+
+	$.ajax( {
+		url: 'https://api.mongolab.com/api/1/databases/task-manager-project/collections/tasks?apiKey=' + api_key,
+	  data: JSON.stringify( {
+			'task_name': task_name,
+			'category': category,
+			'due_date': due_date,
+			'priority': priority
+		}),
+	  type: 'POST',
+	  contentType: 'application/json',
+		success: function(data){
+			window.location.href = 'index.html'
+		},
+		error: function(xhr, status, err){
+			console.log(err)
+		}
+	})
+
+	return false
+}
+
+// GET REQUEST - Tasks
+function getTasks(){
+	$.get('https://api.mongolab.com/api/1/databases/task-manager-project/collections/tasks?apiKey=' + api_key, function(data){
+		var output = '<ul class="list-group">'
+		$.each(data, function(key, data){
+			output += (
+				'<li class="task list-group-item">'+
+				data.task_name +
+				' <span class="due_date">[Due on ' +
+				data.due_date +
+				']</span>'
+			)
+			switch(data.priority){
+				case 'urgent':
+					output += ' <span class="label label-danger">Urgent</span>'
+					break
+				case 'short':
+					output += ' <span class="label label-warning">Short term</span>'
+					break
+				case 'long':
+					output += ' <span class="label label-success">Long term</span>'
+					break
+			}
+			output += (
+				'<div class="pull-right">'+
+					'<a class="btn btn-primary btn-edit-task" data-task-id="' +
+						data._id.$oid +
+						'">Edit</a>' +
+					' ' +
+					'<a class="btn btn-danger btn-delete-task" data-task-id="' +
+						data._id.$oid +
+						'">Delete</a>' +
+					'</div>' +
+				'</li>'
+			)
+		})
+		output += '</ul>'
+		$('#tasks').html(output)
+	})
+}
+
+function setTask(){
+	var task_id = $(this).data('task-id')
+	sessionStorage.setItem('currentTaskId', task_id)
+	window.location.href = "edittask.html"
+	return false
+}
+
+function getTask(){
+	var task_id = sessionStorage.getItem('currentTaskId')
+	$.get('https://api.mongolab.com/api/1/databases/task-manager-project/collections/tasks/' + task_id + '?apiKey=' + api_key, function(data){
+		$('#task_name').val(data.task_name)
+		$('#category').val(data.category)
+		$('#due_date').val(data.due_date)
+		$('#priority').val(data.priority)
+	})
+}
+// PUT REQUEST - Task
+function editTask(){
+	var task_id = sessionStorage.getItem('currentTaskId')
+	var task_name = $('#task_name').val()
+	var category = $('#category').val()
+	var due_date = $('#due_date').val()
+	var priority = $('#priority').val()
+
+	$.ajax( {
+		url: 'https://api.mongolab.com/api/1/databases/task-manager-project/collections/tasks/' + task_id + '?apiKey=' + api_key,
+	  data: JSON.stringify( {
+			'task_name': task_name,
+			'category': category,
+			'due_date': due_date,
+			'priority': priority
+		}),
+	  type: 'PUT',
+	  contentType: 'application/json',
+		success: function(data){
+			window.location.href = 'index.html'
+		},
+		error: function(xhr, status, err){
+			console.log(err)
+		}
+	})
+
+	return false
+}
+
+// DELETE REQUEST - Task
+function deleteTask(){
+	var task_id = $(this).data('task-id')
+
+	$.ajax({
+		url: 'https://api.mongolab.com/api/1/databases/task-manager-project/collections/tasks/' + task_id + '?apiKey=' + api_key,
+		type: 'DELETE',
+		async: true,
+		timeout: 300000,
+		success: function(data){
+			window.location.href = 'index.html'
+		},
+		error: function(xhr, status, err){
+			console.log(err)
+		}
+	})
+
+	return false
+}
+
+
+// GET REQUEST - Categories (as options)
+function getCategoryOptions(){
+	$.get('https://api.mongolab.com/api/1/databases/task-manager-project/collections/categories?apiKey=' + api_key, function(data){
+		var output = ''
+		$.each(data, function(key, data){
+			output += (
+				'<option value="'+
+				data.category_name +
+				'">'+
+				data.category_name +
+				'</option>'
+			)
+		})
+		$('#category').append(output)
+	})
+}
+
+
+// GET REQUEST - Categories (as a list)
 function getCategories(){
 	$.get('https://api.mongolab.com/api/1/databases/task-manager-project/collections/categories?apiKey=' + api_key, function(data){
 		var output = '<ul class="list-group">'
@@ -35,6 +195,7 @@ function getCategories(){
 // POST REQUEST - Category
 function addCategory(){
 	var category_name = $('#category_name').val()
+
 	$.ajax( {
 		url: 'https://api.mongolab.com/api/1/databases/task-manager-project/collections/categories?apiKey=' + api_key,
 	  data: JSON.stringify( {
@@ -49,6 +210,7 @@ function addCategory(){
 			console.log(err)
 		}
 	})
+
 	return false
 }
 
@@ -56,6 +218,7 @@ function addCategory(){
 function editCategory(){
 	var category_id = sessionStorage.getItem('currentCategoryId')
 	var category_name = $('#category_name').val()
+
 	$.ajax( {
 		url: 'https://api.mongolab.com/api/1/databases/task-manager-project/collections/categories/' + category_id + '?apiKey=' + api_key,
 	  data: JSON.stringify( {
@@ -70,6 +233,7 @@ function editCategory(){
 			console.log(err)
 		}
 	})
+
 	return false
 }
 
@@ -78,12 +242,14 @@ function setCategory(){
 	var category_id = $(this).data('category-id')
 	sessionStorage.setItem('currentCategoryId', category_id)
 	window.location.href="editcategory.html"
+
 	return false
 }
 
 // GET REQUEST + GETTER (SESSION) - Category
 function getCategory(){
 	var category_id = sessionStorage.getItem('currentCategoryId')
+
 	$.get('https://api.mongolab.com/api/1/databases/task-manager-project/collections/categories/' + category_id + '?apiKey=' + api_key, function(data){
 		$('#category_name').val(data.category_name)
 	})
@@ -92,6 +258,7 @@ function getCategory(){
 // DELETE REQUEST
 function deleteCategory(){
 	var category_id = $(this).data('category-id')
+
 	$.ajax( {
 		url: 'https://api.mongolab.com/api/1/databases/task-manager-project/collections/categories/' + category_id + '?apiKey=' + api_key,
 	  type: 'DELETE',
@@ -104,5 +271,6 @@ function deleteCategory(){
 			console.log(err)
 		}
 	})
+
 	return false
 }
